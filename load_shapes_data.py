4dd16f029af7fa708d6cd275cdfa806be08bbade
Pranay Namburi <pranayn1999@gmail.com>
2024-10-24 17:08:59 -0400

    format load shapes data

--- load_shapes_data.py
+++ load_shapes_data.py
@@ -6,31 +6,31 @@ from create_tables import create_tables
 from envConfig import GTFS_ROOT_FILE_PATH
 
 def load_shapes_data():
-    try:
-        # Read shapes.txt into a pandas DataFrame
-        df = pd.read_csv(GTFS_ROOT_FILE_PATH + '/shapes.txt')
+  try:
+    # Read shapes.txt into a pandas DataFrame
+    df = pd.read_csv(GTFS_ROOT_FILE_PATH + '/shapes.txt')
 
-        # Create a new session
-        with Session(engine) as session:
-            for _, row in df.iterrows():
-                shape = Shape(
-                    shape_id=row['shape_id'],
-                    shape_pt_lat=row['shape_pt_lat'],
-                    shape_pt_lon=row['shape_pt_lon'],
-                    shape_pt_sequence=row['shape_pt_sequence'],
-                    shape_dist_traveled=row['shape_dist_traveled'] if pd.notna(row['shape_dist_traveled']) else None,
-                    eta_pattern_id=row['eta_pattern_id'] if pd.notna(row['eta_pattern_id']) else None
-                )
-                session.add(shape)
+    # Create a new session
+    with Session(engine) as session:
+      for _, row in df.iterrows():
+        shape = Shape(
+          shape_id=row['shape_id'],
+          shape_pt_lat=row['shape_pt_lat'],
+          shape_pt_lon=row['shape_pt_lon'],
+          shape_pt_sequence=row['shape_pt_sequence'],
+          shape_dist_traveled=row['shape_dist_traveled'] if pd.notna(row['shape_dist_traveled']) else None,
+          eta_pattern_id=row['eta_pattern_id'] if pd.notna(row['eta_pattern_id']) else None
+        )
+        session.add(shape)
 
-            # Commit the session to save the data to the database
-            session.commit()
+      # Commit the session to save the data to the database
+      session.commit()
 
-        print("Shapes data loaded successfully.")
+    print("Shapes data loaded successfully.")
 
-    except Exception as e:
-        print(f"An error occurred: {e}")
+  except Exception as e:
+    print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
-    create_tables()
-    load_shapes_data()
+  create_tables()
+  load_shapes_data()
